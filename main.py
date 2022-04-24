5ffafb6b7461431574b21975c293f6d47a546de2
KojiOchiai <12869331+KojiOchiai@users.noreply.github.com>
2022-04-25 01:27:00 +0900

    add CORS setting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from tkinter import SE
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 from lib import crud, models, schemas
@@ -11,6 +11,18 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+
+origins = {
+    'http://localhost:3000',
+}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 # Dependency
 def get_db():
